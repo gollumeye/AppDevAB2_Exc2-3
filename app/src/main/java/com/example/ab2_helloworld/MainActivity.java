f54e27f7e4e5c8f6ab231382323b0d7646a100a3
Laura Randl <gollumeye@outlook.de>
2022-03-27 17:11:02 +0200

    exc3_logging

--- app/src/main/java/com/example/ab2_helloworld/MainActivity.java
+++ app/src/main/java/com/example/ab2_helloworld/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ab2_helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("debugMessage", "Debug Message Test");
+        Log.e("errorMessage", "Error Message Test");
+        Log.i("infoMessage", "Info Message Test");
+        Log.v("verboseMessage", "Verbose Message Test");
+        Log.w("warnMessage", "Warning Message Test");
     }
 }
